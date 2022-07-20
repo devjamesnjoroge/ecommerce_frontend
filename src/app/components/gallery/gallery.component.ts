758ace39e410c917b8540948767c2cc87e9e243c
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-07-20 07:14:13 +0300

    ft: use a template driven approach on the banners

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -9,6 +9,13 @@ export class GalleryComponent implements OnInit {
 
   constructor() { }
 
+
+  banners: any[] = [
+    {'title': 'Hoodies & Sweats', 'image_url': 'https://res.cloudinary.com/dkz8w5n6k/image/upload/v1658289182/1_yafnei.png'},
+    {'title': 'Graphic & Tees', 'image_url': 'https://res.cloudinary.com/dkz8w5n6k/image/upload/v1658289181/2_v2iqkd.png'},
+    {'title': 'Jeans', 'image_url': 'https://res.cloudinary.com/dkz8w5n6k/image/upload/v1658289181/3_x1z7io.png'}
+  ]
+
   ngOnInit(): void {
   }
 
