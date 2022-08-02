e6eea0cc3c15270b30cd7a1af2f36d8d76ee9a27
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-08-02 17:37:32 +0300

    ft: implement heroku backend for authentication

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://127.0.0.1:8000/api/'
+const baseUrl = 'https://ecommerce-backend-xd.herokuapp.com/api/';
 
 const httpOptions = {
   headers: new HttpHeaders({
