3b8b3f369bc2297e85b6423cfecef6c51b5c347d
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-08-02 13:36:47 +0300

    ft: change product names

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -8,16 +8,16 @@ export class ProductsService {
   constructor() { }
 
   array: any[] = [
-    {'url': 'https://images.unsplash.com/photo-1507253831417-37c43a944f51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 1, 'price': 20},
-    {'url': 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 2, 'price': 35},
-    {'url': 'https://images.unsplash.com/photo-1434510423563-c7e99bbc5bbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 3, 'price': 30},
-    {'url': 'https://images.unsplash.com/photo-1509305717900-84f40e786d82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 4, 'price': 20},
-    {'url': 'https://images.unsplash.com/photo-1520367745676-56196632073f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 5, 'price': 22},
-    {'url': 'https://images.unsplash.com/photo-1591470481729-2bcc11e3acb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 6, 'price': 18},
-    {'url': 'https://images.unsplash.com/photo-1592236943360-f5409792c176?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 7, 'price': 38},
-    {'url': 'https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 8, 'price': 15},
-    {'url': 'https://images.unsplash.com/photo-1531901599143-df5010ab9438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 9, 'price': 20},
-    {'url': 'https://images.unsplash.com/photo-1507114845806-0347f6150324?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Product Name', 'id': 10, 'price': 28}
+    {'url': 'https://images.unsplash.com/photo-1507253831417-37c43a944f51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Denim ripped Jeans', 'id': 1, 'price': 20},
+    {'url': 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Brown leather jacket', 'id': 2, 'price': 35},
+    {'url': 'https://images.unsplash.com/photo-1434510423563-c7e99bbc5bbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Red checked shirt', 'id': 3, 'price': 30},
+    {'url': 'https://images.unsplash.com/photo-1509305717900-84f40e786d82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Cream Mandarin shirt', 'id': 4, 'price': 20},
+    {'url': 'https://images.unsplash.com/photo-1520367745676-56196632073f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Silk pants', 'id': 5, 'price': 22},
+    {'url': 'https://images.unsplash.com/photo-1591470481729-2bcc11e3acb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Full men suit', 'id': 6, 'price': 18},
+    {'url': 'https://images.unsplash.com/photo-1592236943360-f5409792c176?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Vintage shirt', 'id': 7, 'price': 38},
+    {'url': 'https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'black cap', 'id': 8, 'price': 15},
+    {'url': 'https://images.unsplash.com/photo-1531901599143-df5010ab9438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Thick pullneck', 'id': 9, 'price': 20},
+    {'url': 'https://images.unsplash.com/photo-1507114845806-0347f6150324?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&', 'name': 'Casual blazer', 'id': 10, 'price': 28}
   ]
 
 }
